d8d85ff2047bf78177c7d087ef444fd81c659ef2
Gaurav <gauravjoshi20199@gmail.com>
2025-01-10 21:29:35 +0530

    Resource server working. Protecting a demo controller using both spring authorization server and resource server

---
+++ spring-boot-security-oauth-resource-server-1/src/main/java/com/revisit/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.revisit.springsecurity.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${jwks.uri}")
+    private String jwksUri;
+    @Bean
+    public SecurityFilterChain oauthResourceServerFilterChain(HttpSecurity security) throws Exception {
+        security.oauth2ResourceServer(customizer -> {
+            customizer.jwt(customizer2 -> {
+                customizer2.jwkSetUri(jwksUri);
+            });
+        });
+
+        return security.formLogin(Customizer.withDefaults()).
+                authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()).
+                build();
+    }
+}
